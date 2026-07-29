0833cad6c4bd13aec59bc18d3a564ef571e7c0eb
janickkiy <janickiy@mail.ru>
2026-07-29 09:31:05 +0300

    bugfix

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -69,6 +69,10 @@
         .admin-table-card div.dt-container > .row:last-child .pagination {
             margin-bottom: 0;
         }
+        .admin-table-card div.dt-container > .row {
+            margin-left: 0;
+            margin-right: 0;
+        }
         div.dt-container .page-link {
             min-width: 42px;
             padding: .55rem .8rem;
